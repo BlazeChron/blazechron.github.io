cdc742bfdadf80eb540fdd8260a106d70dab6135
BlazeChron <blazechron@JasonsAwesomeComputer.>
2025-11-21 18:49:55 +0800

    Add chains

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,12 @@ import shimaenaga from "../assets/shimaenaga.png";
 import github from "../assets/github-mark.svg";
 import itch from "../assets/itchio-textless-black.svg";
 import Link from 'next/link';
+import ChainScene from './threejs/chains'
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <ChainScene />
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
           <Image
             src={shimaenaga}
